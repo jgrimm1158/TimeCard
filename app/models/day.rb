9147165a6c671fb96f675adf0e7d4bfa2d5cea46
Rich Catalano <rcatalano@companioncorp.com>
2010-11-14 00:17:10 -0700

    :worked enum moved out of Day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,7 +1,3 @@
 class Day < ActiveRecord::Base
   belongs_to :card
-  as_enum :worked, {:Worked => "Worked", :PTO => "PTO", :Half_PTO => "Half_PTO", :Did_Not_Work => "Did_Not_Work", :Paid_Holiday => "Paid_Holiday"}
-  
-  validates_inclusion_of :worked, :in => [:Worked, :PTO, :Half_PTO, :Did_Not_Work, :Paid_Holiday]
-
 end
