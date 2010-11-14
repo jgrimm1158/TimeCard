48338141e1d26d2b21c63c2c51bd9995bdeeb3d9
Rich Catalano <rcatalano@companioncorp.com>
2010-11-14 00:17:34 -0700

    Fixed to make rake migrations not fail

--- db/migrate/20101108070826_add_references.rb
+++ db/migrate/20101108070826_add_references.rb
@@ -1,14 +1,14 @@
 class AddReferences < ActiveRecord::Migration
   def self.up
-    remove_column :users, :department_id
-    remove_column :cards, :user_id
-    remove_column :days, :card_id
-    remove_column :departments, :manager_id
-    
-    add_column :users, :references, :department
-    add_column :cards, :references, :user
-    add_column :days, :references, :card
-    add_column :departments, :references, :manager
+    # remove_column :users, :department_id
+    # remove_column :cards, :user_id
+    # remove_column :days, :card_id
+    # remove_column :departments, :manager_id
+    # 
+    # add_column :users, :reference, :department
+    # add_column :cards, :reference, :user
+    # add_column :days, :reference, :card
+    # add_column :departments, :reference, :manager
   end
 
   def self.down
