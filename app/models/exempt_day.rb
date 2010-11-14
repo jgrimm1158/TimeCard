88ce4485d63196241922efb229157da058632854
Rich Catalano <rcatalano@companioncorp.com>
2010-11-14 00:15:21 -0700

    Adding hourly and exempt days

---
+++ app/models/exempt_day.rb
@@ -0,0 +1,8 @@
+class ExemptDay < Day
+  enum_values = [:worked, :PTO, :half_PTO, :did_not_work, :paid_holiday]
+  symbolize :worked, :in => enum_values
+  validates_inclusion_of :worked, :in => enum_values
+  
+  default_value_for :worked, :worked
+  
+end
